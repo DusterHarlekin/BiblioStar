505a1d36a64456fdc50e41e32f4b5655a9212abe
Ary <areamny@gmail.com>
2024-07-06 17:57:18 -0400

    Cambios ary

--- client/src/router/routes.js
+++ client/src/router/routes.js
@@ -1,5 +1,11 @@
 const routes = [
-  
+  {
+    path: '/',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [
+      { path: '', component: () => import('pages/IndexPage.vue') }
+    ]
+  },
   {
     path: '/login',
     component: () => import('layouts/LoginLayout.vue'),
